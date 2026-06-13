1401549ea190fe6eef3910b90fb75268d43e3048
Som Dutt Rai <somlibert357@gmail.com>
2026-06-13 15:31:09 +0530

    Add Runnable interface example

---
+++ week1-thread-basics/ThreadUsingRunnable.java
@@ -0,0 +1,19 @@
+class Task implements Runnable {
+
+    @Override
+    public void run() {
+        for(int i = 1; i <= 5; i++) {
+            System.out.println(Thread.currentThread().getName() + " : " + i);
+        }
+    }
+}
+
+public class ThreadUsingRunnable {
+
+    public static void main(String[] args) {
+
+        Thread t1 = new Thread(new Task());
+
+        t1.start();
+    }
+}
